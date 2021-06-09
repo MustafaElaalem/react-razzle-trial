95d4a2b347455270c23395230a64e64e35cf20fe
MOSTAFA MOHAMED <m.mohamed@penta-b.com>
2021-06-09 22:24:33 +0200

    migrate to razzle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from 'react';
 import Home from './components/Home/Home';
 import NavBar from "./components/NavBar/NavBar";
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import ForgetPassword from "./components/RegisterLogin/ForgetPassword";
+import RegisterLoginContainer from "./components/RegisterLogin/RegisterLoginContainer";
+import FoundForm from "./components/foundForm/mainComponent";
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 
 const App = () => (
@@ -10,6 +13,11 @@ const App = () => (
     <main>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/foundForm" component={FoundForm} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/RegisterLogin" component={RegisterLoginContainer} />
+        <Route exact path="/forgetPassword" component={ForgetPassword} />
+        <Redirect from="/home" to="/" />
       </Switch>
     </main>
   </>
